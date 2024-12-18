bb2cad35c71eaecd4d4d527d6d68d9f6c6ec5a1f
Maxim Shapovalov <syndex@list.ru>
2024-12-18 17:44:30 +0300

    GET filtered functionality implemented

--- src/main/java/com/github/syndexmx/socksbase/controllers/GetFilteredSocksController.java
+++ src/main/java/com/github/syndexmx/socksbase/controllers/GetFilteredSocksController.java
@@ -3,6 +3,8 @@ package com.github.syndexmx.socksbase.controllers;
 import com.github.syndexmx.socksbase.services.SocksFilteredCollectionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,14 +22,32 @@ public class GetFilteredSocksController {
     }
 
     @GetMapping(path = "api/socks")
-    Long getFilteredAmount(@RequestParam String colour,
-                           @RequestParam(required = false) Integer lessThan,
-                           @RequestParam(required = false) Integer moreThan,
-                           @RequestParam(required = false) Integer equal){
+    ResponseEntity<Long> getFilteredAmount(@RequestParam String colour,
+                                           @RequestParam(required = false) Integer lessThan,
+                                           @RequestParam(required = false) Integer moreThan,
+                                           @RequestParam(required = false) Integer equal){
         if (equal != null ) {
-            return socksFilteredCollectionService.getFilteredAmount(colour, equal);
+            return new ResponseEntity<>(socksFilteredCollectionService.getFilteredAmount(colour, equal),
+                    HttpStatus.OK);
         }
-        return socksFilteredCollectionService.getFilteredAmount(colour, moreThan, lessThan);
+        if (lessThan != null && moreThan !=null) {
+            return new ResponseEntity<>(socksFilteredCollectionService
+                    .getFilteredAmount(colour, moreThan, lessThan),
+                    HttpStatus.OK);
+        }
+        if (lessThan != null) {
+            return new ResponseEntity<>(socksFilteredCollectionService
+                    .getFilteredAmount(colour, 0, lessThan),
+                    HttpStatus.OK);
+        }
+        if (moreThan != null) {
+            return new ResponseEntity<>(socksFilteredCollectionService
+                    .getFilteredAmount(colour, moreThan, 100),
+                    HttpStatus.OK);
+        }
+        return new ResponseEntity<>(socksFilteredCollectionService
+                .getFilteredAmount(colour, 0, 100),
+                HttpStatus.OK);
     }
 
 }
