0c6bffbc02062ba46ccb19d5cda7dca0536e85b9
Maxim Shapovalov <syndex@list.ru>
2024-12-19 22:33:02 +0300

    Services logs added

--- src/main/java/com/github/syndexmx/socksbase/services/impl/CorrectionSocksServiceImpl.java
+++ src/main/java/com/github/syndexmx/socksbase/services/impl/CorrectionSocksServiceImpl.java
@@ -3,10 +3,12 @@ package com.github.syndexmx.socksbase.services.impl;
 import com.github.syndexmx.socksbase.model.Socks;
 import com.github.syndexmx.socksbase.repositories.SocksRepositoryService;
 import com.github.syndexmx.socksbase.services.CorrectionSocksService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CorrectionSocksServiceImpl implements CorrectionSocksService {
 
     private final SocksRepositoryService auxRepository;
@@ -17,6 +19,7 @@ public class CorrectionSocksServiceImpl implements CorrectionSocksService {
 
     @Override
     public Socks correctSocks(Socks socksToFix) {
+        log.info("CorrectionSocksServiceImpl : correctSocks" + socksToFix.toString());
         Socks savedSocks = auxRepository.save(socksToFix);
         return savedSocks;
     }
