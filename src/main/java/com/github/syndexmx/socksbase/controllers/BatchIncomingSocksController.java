24e57605e48ca46fc16a63536c568aea48dcd688
Maxim Shapovalov <syndex@list.ru>
2024-12-19 17:40:49 +0300

    Batch income Controller

---
+++ src/main/java/com/github/syndexmx/socksbase/controllers/BatchIncomingSocksController.java
@@ -0,0 +1,40 @@
+package com.github.syndexmx.socksbase.controllers;
+
+import com.github.syndexmx.socksbase.model.Socks;
+import com.github.syndexmx.socksbase.services.IncomingSocksService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.github.syndexmx.socksbase.controllers.csvtools.csvParser.getAllSocksFromBatch;
+
+@RestController
+@RequestMapping
+@Slf4j
+public class BatchIncomingSocksController {
+
+    private final IncomingSocksService incomingSocksService;
+
+    public BatchIncomingSocksController(@Autowired IncomingSocksService incomingSocksService) {
+        this.incomingSocksService = incomingSocksService;
+    }
+
+    @PostMapping(path = "api/socks/batch")
+    ResponseEntity<Object> batchIncome(@RequestBody String csvBody) {
+        log.info("POST @api/socks/batch : " + csvBody.toString());
+        List<Socks> incomingSocksList = getAllSocksFromBatch(csvBody);
+        List<Socks> updatedList = incomingSocksList.stream().map(
+                (Socks socks) -> {
+                    return incomingSocksService.addSocks(socks);
+                }).toList();
+        return new ResponseEntity<>(updatedList, HttpStatus.ACCEPTED);
+    }
+
+}
---
+++ src/main/java/com/github/syndexmx/socksbase/controllers/csvtools/csvParser.java
@@ -0,0 +1,45 @@
+package com.github.syndexmx.socksbase.controllers.csvtools;
+
+import com.github.syndexmx.socksbase.model.Socks;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+public class csvParser {
+
+    public static List<Socks> getAllSocksFromBatch(String csvBody) {
+        List<Socks> list = new ArrayList<>();
+        String lines[] = csvBody.split("\\r?\\n");
+        List<String> csvStrings = Arrays.asList(lines);
+        log.info("CSV " + csvStrings.size() + " lines.");
+        csvStrings.forEach(line -> {if (line.length() > 0) {
+            list.add(csvLineToSocks(line));}});
+        return list;
+    }
+
+    private static Socks csvLineToSocks(String csvLine) {
+        String separator = ",";
+        if (csvLine.contains(";")) {
+            separator = ";";
+        }
+        String[] cells = csvLine.split(separator);
+        if (cells.length != 3) {
+            throw new RuntimeException("Improper CSV line format.");
+        }
+        Long typeId = 0L;
+        String colour = cells[0];
+        Integer cotton = Integer.parseInt(cells[1]);
+        Integer amount = Integer.parseInt(cells[1]);
+        Socks socks = Socks.builder()
+                .typeId(typeId)
+                .colour(colour)
+                .cotton(cotton)
+                .amount(amount)
+                .build();
+        return socks;
+    }
+
+}
